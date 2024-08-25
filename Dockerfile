a178270b04aeaa51d31e6d12feff318aa00bda6d
Mitchell Jensen <mjensen98@gmail.com>
2024-08-25 04:09:05 +0000

    Added tests

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,2 @@
 FROM python:3.12-slim-bookworm
 COPY --from=ghcr.io/astral-sh/uv:0.3.3 /uv /bin/uv
-WORKDIR /app
-COPY . .
--- src/book_tracker/tests.py
+++ src/book_tracker/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import pytest
+from book_tracker.models import Book
 
-# Create your tests here.
+@pytest.mark.django_db
+class TestBookModel:
+    def test_create_book(self):
+        book = Book.objects.create(title="Stick Control", author="George Lawrence Stone")
+        assert book.title == "Stick Control"
+        assert book.author == "George Lawrence Stone"
+        assert Book.objects.count() == 1
