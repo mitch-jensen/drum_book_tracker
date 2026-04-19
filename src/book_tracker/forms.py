ffe6ebb15b51995e50d208bf4bf52baeccbaf869
Mitch <mjensen98@gmail.com>
2026-04-19 15:43:36 +1000

    add preliminary styling via django-crispy-forms

--- src/book_tracker/forms.py
+++ src/book_tracker/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout
 from django import forms
 
 from book_tracker.models import Author
@@ -7,3 +9,11 @@ class AuthorForm(forms.ModelForm):
     class Meta:  # noqa: D106
         model = Author
         fields = ("first_name", "last_name")
+
+    def __init__(self, *args: object, **kwargs: object) -> None:  # noqa: D107
+        super().__init__(*args, **kwargs)  # pyright: ignore[reportArgumentType]
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout("first_name", "last_name")
+        for field in self.fields.values():
+            field.widget.attrs.setdefault("class", "form-control")
